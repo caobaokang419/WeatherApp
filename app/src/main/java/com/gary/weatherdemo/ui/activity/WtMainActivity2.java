0fda6c29c1b824b5dfbc11ab03962345bed57c3b
gary.cao <gary.cao@example.com>
2019-01-21 23:56:09 +0800

    viewpager viewmodel code refactoring

--- app/src/main/java/com/gary/weatherdemo/ui/activity/WtMainActivity2.java
+++ app/src/main/java/com/gary/weatherdemo/ui/activity/WtMainActivity2.java
@@ -1,13 +1,22 @@
 package com.gary.weatherdemo.ui.activity;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.example.commonui.ActionBar;
 import com.example.commonui.PageIndicatorView;
 import com.gary.weatherdemo.R;
 import com.gary.weatherdemo.model.CityBean;
 import com.gary.weatherdemo.ui.adapter.CityPagerAdapter;
+import com.gary.weatherdemo.utils.LogUtils;
+import com.gary.weatherdemo.utils.WeatherUtils;
+import com.gary.weatherdemo.viewmodel.MainActivity2ViewModel;
+import com.gary.weatherdemo.viewmodel.MainActivityViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,13 +26,12 @@ import java.util.List;
  * 左右滑动切换：不同城市天气信息：ViewPager+PagerAdapter
  */
 public class WtMainActivity2 extends AppCompatActivity {
+    private MainActivity2ViewModel viewModel;
+    private ActionBar actionBar;
+
     private ViewPager viewPager;
     private CityPagerAdapter cityPagerAdapter;
     private PageIndicatorView pageIndicatorView;
-    private String[] cityNames =
-            new String[]{"深圳", "西安", "合肥", "武汉", "巢湖市", "北京", "上海", "广州", "成都", "南京", "杭州", "苏州"};
-    private String[] cityAdcode =
-            new String[]{"440300", "610100", "340100", "420100", "340181", "110000", "310000", "440100", "510100", "320100", "330100", "320500"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +41,55 @@ public class WtMainActivity2 extends AppCompatActivity {
     }
 
     private void initView(){
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewModel = ViewModelProviders.of(this).get(MainActivity2ViewModel.class);
 
-        List<CityBean> cityBeanList = new ArrayList<>();
-        for (int i = 0; i < cityNames.length; i++) {
-            CityBean CityBean = new CityBean(cityNames[i],cityAdcode[i]);
-            cityBeanList.add(CityBean);
-        }
-        cityPagerAdapter = new CityPagerAdapter(this,cityBeanList);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        cityPagerAdapter = new CityPagerAdapter(this,viewModel.getCityInfoList());
         viewPager.setAdapter(cityPagerAdapter);
 
         pageIndicatorView = findViewById(R.id.page_indicatior);
         pageIndicatorView.registerPageChangeListener(viewPager);
+
+        initActionBar();
+        updateCityTitleView();
+    }
+
+    private void initActionBar() {
+        actionBar = findViewById(R.id.action_bar);
+        actionBar.setLeftOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LogUtils.d("onClickedLeftBtn()");
+                WeatherUtils.startActivity(getApplicationContext(), WtSearchActivity.class);
+            }
+        });
+
+        actionBar.setRightOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LogUtils.d("onClickedRightBtn()");
+                WeatherUtils.startActivity(getApplicationContext(), WtSettingActivity.class);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        if (viewModel != null) {
+            viewModel.loadCurCityInfo(new CityBean("深圳", "440300"));
+        }
+        super.onResume();
+    }
+
+    private void updateCityTitleView() {
+        viewModel.getCurCityInfo().observe(this, new Observer<CityBean>() {
+            @Override
+            public void onChanged(@Nullable CityBean cityBean) {
+                if (null != actionBar) {
+                    actionBar.setTitle(cityBean.adrName);
+                }
+                viewModel.queryCityWeather(cityBean.adcCode);
+            }
+        });
     }
 }
--- app/src/main/java/com/gary/weatherdemo/viewmodel/MainActivity2ViewModel.java
+++ app/src/main/java/com/gary/weatherdemo/viewmodel/MainActivity2ViewModel.java
@@ -4,8 +4,11 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.MutableLiveData;
 import android.databinding.ObservableField;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
 
+import com.example.commonui.PageIndicatorView;
 import com.gary.weatherdemo.model.CityBean;
 import com.gary.weatherdemo.model.DayForecastBean;
 import com.gary.weatherdemo.model.LiveWeatherBean;
@@ -26,16 +29,33 @@ import io.reactivex.schedulers.Schedulers;
 
 
 public class MainActivity2ViewModel extends AndroidViewModel {
+    private int pageCount;
+    private int pageSelectedIndex;
+    private PageChangeListener pageChangeListener = new PageChangeListener();
+
     public final ObservableField<CityWeatherRecyclerAdapter> weatherAdapter = new ObservableField<>();
     private final CityWeatherRecyclerAdapter adapter;
     private MutableLiveData<CityBean> curCityBean = new MutableLiveData<>();
+    private MutableLiveData<List<CityBean>> cityBeanList = new MutableLiveData<>();
 
     private List<BaseItemBean> itemDataList = new ArrayList<>();
     private MutableLiveData<LiveWeatherBean> liveWeatherData = new MutableLiveData<>();
 
+    private String[] cityNames =
+            new String[]{"深圳", "西安", "合肥", "武汉", "巢湖市", "北京", "上海", "广州", "成都", "南京", "杭州", "苏州"};
+    private String[] cityAdcode =
+            new String[]{"440300", "610100", "340100", "420100", "340181", "110000", "310000", "440100", "510100", "320100", "330100", "320500"};
+
     public MainActivity2ViewModel(@NonNull Application application) {
         super(application);
         adapter = new CityWeatherRecyclerAdapter();
+
+        List<CityBean> data = new ArrayList<>();
+        for (int i = 0; i < cityNames.length; i++) {
+            CityBean CityBean = new CityBean(cityNames[i],cityAdcode[i]);
+            data.add(CityBean);
+        }
+        cityBeanList.setValue(data);
     }
 
     public void queryCityWeather(String adcode) {
@@ -81,15 +101,47 @@ public class MainActivity2ViewModel extends AndroidViewModel {
                 });
     }
 
-    public MutableLiveData<LiveWeatherBean> getLiveWeatherData() {
-        return liveWeatherData;
-    }
-
-    public void loadCurCityInfo() {
-        curCityBean.setValue(new CityBean("深圳", "440300"));
+    public void loadCurCityInfo(CityBean cityBean) {
+        curCityBean.setValue(cityBean);
     }
 
     public MutableLiveData<CityBean> getCurCityInfo() {
         return curCityBean;
+    }
+
+    public List<CityBean> getCityInfoList() {
+        return cityBeanList.getValue();
+    }
+
+    private void updateCurPageIndex(int index) {
+        pageSelectedIndex = index;
+        loadCurCityInfo(cityBeanList.getValue().get(index));
+    }
+
+    private void updatePageCount(int count) {
+        pageCount = count;
+        //updateView();
+    }
+
+    class PageChangeListener implements ViewPager.OnPageChangeListener {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            updateCurPageIndex(position);
+        }
+    }
+
+    public void registerPageChangeListener(ViewPager viewPager) {
+        viewPager.addOnPageChangeListener(pageChangeListener);
+        updatePageCount(viewPager.getAdapter().getCount());
     }
 }
