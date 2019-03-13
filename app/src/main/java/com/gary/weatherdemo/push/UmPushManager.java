1bc352468a7ddbd4c0a22cf08dc11848a9233ca0
gary.cao <gary.cao@example.com>
2019-03-13 23:23:57 +0800

    umeng push code refactoring

--- app/src/main/java/com/gary/weatherdemo/push/UmPushManager.java
+++ app/src/main/java/com/gary/weatherdemo/push/UmPushManager.java
@@ -43,61 +43,60 @@ public class UmPushManager {
         PushAgent mPushAgent = PushAgent.getInstance(context);
 
         //注册推送服务，每次调用register方法都会回调该接口
-        mPushAgent.register(new IUmengRegisterCallback() {
-
-            @Override
-            public void onSuccess(String deviceToken) {
-                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
-                //注册成功：deviceToken：-------->  AvMK_CTS9yPTiSuvEqQ20I7JLvqM5gBho_RFCISzBU50
-                Log.i(TAG, "注册成功：deviceToken：-------->  " + deviceToken);
-            }
-
-            @Override
-            public void onFailure(String s, String s1) {
-                Log.e(TAG, "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
-            }
-        });
-
-        UmengMessageHandler messageHandler = new UmengMessageHandler() {
-
-            /**
-             * 自定义通知栏样式的回调方法
-             */
-            @Override
-            public Notification getNotification(Context context, UMessage msg) {
-                switch (msg.builder_id) {
-                    case 1:
-                        Notification.Builder builder = new Notification.Builder(context);
-                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(),
-                                R.layout.notification_um_push_view);
-                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
-                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
-                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
-                        myNotificationView.setImageViewResource(R.id.notification_small_icon,
-                                getSmallIconId(context, msg));
-                        builder.setContent(myNotificationView)
-                                .setSmallIcon(getSmallIconId(context, msg))
-                                .setTicker(msg.ticker)
-                                .setAutoCancel(true);
-
-                        return builder.getNotification();
-                    default:
-                        //默认为0，若填写的builder_id并不存在，也使用默认。
-                        return super.getNotification(context, msg);
-                }
-            }
-        };
-        mPushAgent.setMessageHandler(messageHandler);
-
-        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
-
-            @Override
-            public void dealWithCustomAction(Context context, UMessage msg) {
-                Log.e(TAG, "click");
-            }
-
-        };
-
-        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+        mPushAgent.register(mIUmengRegisterCallback);
+        mPushAgent.setMessageHandler(mMessageHandler);
+        mPushAgent.setNotificationClickHandler(mNotificationClickHandler);
     }
+
+    static IUmengRegisterCallback mIUmengRegisterCallback = new IUmengRegisterCallback() {
+
+        @Override
+        public void onSuccess(String deviceToken) {
+            //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
+            //注册成功：deviceToken：-------->  AvMK_CTS9yPTiSuvEqQ20I7JLvqM5gBho_RFCISzBU50
+            Log.i(TAG, "注册成功：deviceToken：-------->  " + deviceToken);
+        }
+
+        @Override
+        public void onFailure(String s, String s1) {
+            Log.e(TAG, "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+        }
+    };
+
+    /**
+     * 自定义通知栏样式的回调方法
+     */
+    static UmengMessageHandler mMessageHandler = new UmengMessageHandler() {
+        @Override
+        public Notification getNotification(Context context, UMessage msg) {
+            switch (msg.builder_id) {
+                case 1:
+                    Notification.Builder builder = new Notification.Builder(context);
+                    RemoteViews myNotificationView = new RemoteViews(context.getPackageName(),
+                            R.layout.notification_um_push_view);
+                    myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+                    myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+                    myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
+                    myNotificationView.setImageViewResource(R.id.notification_small_icon,
+                            getSmallIconId(context, msg));
+                    builder.setContent(myNotificationView)
+                            .setSmallIcon(getSmallIconId(context, msg))
+                            .setTicker(msg.ticker)
+                            .setAutoCancel(true);
+
+                    return builder.getNotification();
+                default:
+                    //默认为0，若填写的builder_id并不存在，也使用默认。
+                    return super.getNotification(context, msg);
+            }
+        }
+    };
+
+    static UmengNotificationClickHandler mNotificationClickHandler = new UmengNotificationClickHandler() {
+        @Override
+        public void dealWithCustomAction(Context context, UMessage msg) {
+            Log.e(TAG, "click");
+        }
+
+    };
 }
