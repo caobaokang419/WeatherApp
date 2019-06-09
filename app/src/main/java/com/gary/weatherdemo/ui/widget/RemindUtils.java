a254be5e4bd7b87bf2aa8af4ad5841a143df7ab9
gary.cao <gary.cao@example.com>
2019-06-09 21:55:13 +0800

    add weather remind infos 2
    Signed-off-by: gary.cao <gary.cao@example.com>

--- app/src/main/java/com/gary/weatherdemo/ui/widget/RemindUtils.java
+++ app/src/main/java/com/gary/weatherdemo/ui/widget/RemindUtils.java
@@ -3,11 +3,13 @@ package com.gary.weatherdemo.ui.widget;
 import com.gary.weatherdemo.bean.WeatherTypeBean;
 import com.gary.weatherdemo.bean.WeatherTypeBean.WeatherType;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class RemindUtils {
-    enum RemindType {
+    public enum RemindType {
         Sunny(0),
         Cloudy(1),
         Rain(3),
@@ -24,18 +26,36 @@ public class RemindUtils {
         }
     }
 
-    private static final Map<WeatherType, RemindType> remindTypeMap;
+    private Map<WeatherType, RemindType> remindTypeMap;
+    private Map<RemindType, ArrayList<String>> remindDataMap;
+
+    private static final ArrayList<String> remindSunny;
+    static {
+        remindSunny = new ArrayList<>();
+        remindSunny.add("你若安好，便是晴天");
+        remindSunny.add("晴带雨伞，饱带干粮");
+    }
+
+    private static final ArrayList<String> remindRain;
+    static {
+        remindRain = new ArrayList<>();
+        remindRain.add("下雨了，记得带伞");
+        remindRain.add("下雨了，不用上班了么");
+    }
+
+    private static final ArrayList<String> remindCloud;
 
     static {
-        remindTypeMap = new HashMap<>();
-        remindTypeMap.put(WeatherType.Sunny, RemindType.Sunny);
-        remindTypeMap.put(WeatherType.Cloudy, RemindType.Cloudy);
-        remindTypeMap.put(WeatherType.RainSmall, RemindType.Rain);
-        remindTypeMap.put(WeatherType.RainMedium, RemindType.Rain);
-        remindTypeMap.put(WeatherType.RainBig, RemindType.Rain);
-        remindTypeMap.put(WeatherType.SnowSmall, RemindType.Snow);
-        remindTypeMap.put(WeatherType.SnowMedium, RemindType.Snow);
-        remindTypeMap.put(WeatherType.SnowSmall, RemindType.Snow);
+        remindCloud = new ArrayList<>();
+        remindCloud.add("阴天，保持好心情");
+        remindCloud.add("多云了，晴天还会远么");
+    }
+
+    private static final ArrayList<String> remindSnow;
+
+    static {
+        remindSnow = new ArrayList<>();
+        remindSnow.add("下雪了，记得收衣服");
     }
 
     private static final RemindUtils ourInstance = new RemindUtils();
@@ -46,17 +66,39 @@ public class RemindUtils {
 
 
     private RemindUtils() {
+        initRemindData();
+    }
 
+    private void initRemindData(){
+        remindTypeMap = new HashMap<>();
+        remindTypeMap.put(WeatherType.Sunny, RemindType.Sunny);
+        remindTypeMap.put(WeatherType.Cloudy, RemindType.Cloudy);
+        remindTypeMap.put(WeatherType.RainSmall, RemindType.Rain);
+        remindTypeMap.put(WeatherType.RainMedium, RemindType.Rain);
+        remindTypeMap.put(WeatherType.RainBig, RemindType.Rain);
+        remindTypeMap.put(WeatherType.SnowSmall, RemindType.Snow);
+        remindTypeMap.put(WeatherType.SnowMedium, RemindType.Snow);
+        remindTypeMap.put(WeatherType.SnowSmall, RemindType.Snow);
+
+        remindDataMap = new HashMap<>();
+        remindDataMap.put(RemindType.Sunny,remindSunny);
+        remindDataMap.put(RemindType.Cloudy,remindCloud);
+        remindDataMap.put(RemindType.Rain,remindRain);
+        remindDataMap.put(RemindType.Snow,remindSnow);
     }
 
     public String getRemindByWeatherType(WeatherTypeBean.WeatherType type) {
         RemindType remindType = remindTypeMap.get(type);
-        return getRemindByRemindType(remindType);
+        ArrayList<String> remindDatas = remindDataMap.get(remindType);
+        return getRandomRemindString(remindDatas);
     }
 
-    private String getRemindByRemindType(RemindType remindType){
-
-
-        return null;
+    private String getRandomRemindString(ArrayList<String> strings) {
+        if (strings == null || strings.size() <= 0) {
+            return null;
+        }
+        Random rand = new Random();
+        int index = rand.nextInt(strings.size());
+        return strings.get(index);
     }
 }
